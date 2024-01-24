7b0e9de72901c43d9d7b8355b6ff11cbc729138c
Juhani Manninen <juhani.manninen00@gmail.com>
2024-01-24 12:56:23 +0200

    Added 404 page

--- server.js
+++ server.js
@@ -97,6 +97,10 @@ app.get('/', async (req, res) => {
   }
 });
 
+app.get('*', function(req, res){
+  res.render("404");
+});
+
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
